10996f21b4fce0ecb650c4d86be02537429a6b70
Nathishwar-prog <nathishwarc@gmail.com>
2026-03-10 06:42:26 +0530

    Day Three of the python course

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Day 1 : Basics of Variables and Data types 
 
 Day 2 : Operators and control flow
+
+Day 3 : Control flow and Loops
--- Variables.py
+++ Variables.py
@@ -2,3 +2,24 @@ name = "John"
 age = 20
 height = 5.9
 is_student = True
+
+
+# For loop , while loop
+
+# for i in range(1,11):
+#     print(i)
+
+while True:
+    user = int(input("Guess the no between 1 to 11:"))
+    if user == 10 :
+        print("Your are won the game !")
+        break
+        
+    print("Youe guessed the wrong no !")
+    
+    
+    
+        
+    
+    
+    
